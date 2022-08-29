d45c111fd9c29f707b31adfd042e8c7ac76b8f09
horikawa <kosei9165@gmail.com>
2022-08-29 18:46:14 +0900

    aaa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,10 +16,11 @@ public class Main {
     }
     void renderBanner() {
         int resize = 0;
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1) &&
-                wasInitialized() && resize > 0 )
-        {
+        final boolean isMacOs = platform.toUpperCase().indexOf("MAC") > -1;
+        final boolean isIE = browser.toUpperCase().indexOf("IE") > -1;
+        final boolean wasResized = resize > 0;
+
+        if (isMacOs && isIE && wasInitialized() && wasResized) {
             // do something
         }
     }
