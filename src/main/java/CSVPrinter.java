ec08cd230cd765bde28c0744260349d18dfc99de
horikawa <kosei9165@gmail.com>
2022-08-29 15:45:53 +0900

    change variable

--- src/main/java/CSVPrinter.java
+++ src/main/java/CSVPrinter.java
@@ -1,6 +1,5 @@
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,12 +7,12 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class CSVPrinter {
-    Reader it;
+    Reader reader;
     public CSVPrinter(String fileName) throws FileNotFoundException {
-        it = new FileReader(fileName);
+        reader = new FileReader(fileName);
     }
     public Iterable<CSVRecord> read() throws IOException {
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(this.in);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(this.reader);
         return records;
     }
 
