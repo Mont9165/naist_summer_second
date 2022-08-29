fba31be2dc3143f1656ccc65bacfd67a5d3077b7
horikawa <kosei9165@gmail.com>
2022-08-29 18:18:40 +0900

    aaa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        /*
         CSVPrinter csvPrint = new CSVPrinter("src/main/sample.csv");
         for (CSVRecord record : csvPrint.read()){
             System.out.println((record.get(0)));
         }
+        */
     }
 }
