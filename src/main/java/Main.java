e26da5fb9ecdd8a43043aa1825bc9355e93ac692
horikawa <kosei9165@gmail.com>
2022-08-29 15:53:39 +0900

    move Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CSVPrinter csvPrinter = new CSVPrinter("src/main/sample.csv");
+        for (CSVRecord record : csvPrinter.read()){
+            System.out.println((record.get(0)));
+        }
+    }
+}
